65dc4f705e5ecfd67e0a49aef700a7484c0f8165
Vardaan Aggarwal <vardaanaggarwalofficial@gmail.com>
2022-06-01 14:02:31 +0530

    Added Sorting (bubble sort)

---
+++ Sorting/src/com/company/BubbleSort.java
@@ -0,0 +1,28 @@
+package com.company;
+
+import java.util.Arrays;
+
+public class BubbleSort {
+    public static void main(String[] args) {
+	// write your code here
+        int[] arr = {5, 4, 3, 2, 1};
+        bubble(arr);
+        System.out.println(Arrays.toString(arr));
+    }
+    static void bubble(int[] arr){
+        for (int i = 0; i < arr.length - 1; i++) {
+            boolean swapped = false;
+            for (int j = 1; j < arr.length - i; j++) {
+                if (arr[j] < arr[j-1]){
+                    int temp = arr[j];
+                    arr[j] = arr[j-1];
+                    arr[j-1] = temp;
+                    swapped = true;
+                }
+            }
+            if (!swapped){
+                break;
+            }
+        }
+    }
+}

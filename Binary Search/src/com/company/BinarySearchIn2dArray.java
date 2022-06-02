78a10a3870299659c55fb1c529863dbf8866818f
Vardaan Aggarwal <vardaanaggarwalofficial@gmail.com>
2022-06-02 23:44:24 +0530

    Add binary search in 2D array

---
+++ Binary Search/src/com/company/BinarySearchIn2dArray.java
@@ -0,0 +1,10 @@
+package com.company;
+
+public class BinarySearchIn2dArray {
+    public static void main(String[] args) {
+
+    }
+//    static int[] search(int[][]arr, int target){
+//
+//    }
+}
